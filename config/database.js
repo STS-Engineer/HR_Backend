15668696e27c878ff24dce56fa09a84b34e6619a
STS-Engineer <administration.sts@avocarbon.com>
2024-08-05 16:11:22 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,12 +1,11 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-pool.on("connect", () => {
-  console.log("Connected to the database");
+  user: 'adminavo',
+  host: 'avo-adb-001.postgres.database.azure.com',
+  database: 'leave-management',
+  password: '$#fKcdXPg4@ue8AW', 
+  port: 5432,
 });
 
 module.exports = pool;
