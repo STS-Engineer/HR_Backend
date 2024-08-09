e8cf98947c01ad158566bed02de9c5163b2037ba
STS-Engineer <administration.sts@avocarbon.com>
2024-08-09 14:27:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use("/mission-requests", missionRequestsRouter);
 app.use("/authorization-requests", authorizationRouter);
 app.use("/document-requests", docRequestsRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
