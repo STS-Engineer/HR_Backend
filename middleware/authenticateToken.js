4a7d863187289727eceaaadb733a50f5b1810610
STS-Engineer <administration.sts@avocarbon.com>
2024-08-19 11:05:57 +0100

    Update authenticateToken.js

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -1,14 +1,18 @@
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.header("Authorization");
-  if (!authHeader) {
-    return res.sendStatus(401);
+const jwt = require("jsonwebtoken");
+
+const authenticate = (req, res, next) => {
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "No token provided." });
   }
 
-  const token = authHeader.split(" ")[1];
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
+  jwt.verify(token, "jwtSecret", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Failed to authenticate token." });
+    }
+    req.user = decoded.user;
     next();
   });
 };
+
+module.exports = { authenticate };
